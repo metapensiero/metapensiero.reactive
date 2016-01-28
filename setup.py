ec75b5b9bff48d1ab48aae2ce4da7977cdf20fb9
Alberto Berti <alberto@metapensiero.it>
2016-01-28 12:48:09 +0100

    Update dependency on metapensiero.signal

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     namespace_packages=['metapensiero'],
 
     install_requires=['setuptools',
-                      'metapensiero.signal>=0.3',
+                      'metapensiero.signal>=0.6',
                       'six'],
     extras_require={'dev': ['metapensiero.tool.bump_version', 'docutils']},
     setup_requires=['pytest-runner'],
