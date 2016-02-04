957447a932108d1f318cf87ae1bb9fcab774546d
Alberto Berti <alberto@metapensiero.it>
2016-02-04 04:56:06 +0100

    Fix stop()

--- src/metapensiero/reactive/computation.py
+++ src/metapensiero/reactive/computation.py
@@ -124,9 +124,11 @@ class Computation(object):
 
     def stop(self):
         if not self.stopped:
+            self.stopped = True
             self.invalidate()
             self._tracker._computations.remove(self)
-            self.stopped = True
+            self._func = None
+            self._tracker = None
 
     def _on_parent_invalidated(self, parent):
         self.stop()
