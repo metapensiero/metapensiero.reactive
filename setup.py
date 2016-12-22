9a06fa57a9b6b2843060f697182d61301b3865af
Lele Gaifax <lele@metapensiero.it>
2016-12-22 14:45:35 +0100

    Add Python 3.6 trove classifier

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License (GPL)",
        ],
